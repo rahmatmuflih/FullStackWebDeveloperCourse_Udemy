27b1793871703337b84c0c7538fbbd8302cdaaf7
Muflih <mufliharifinrahmat@gmail.com>
2023-11-12 17:14:15 +0700

    Refine section 17 challange

--- section_17/challange/Dicee_Challenge___Starting_Files/Dicee Challenge - Starting Files/script.js
+++ section_17/challange/Dicee_Challenge___Starting_Files/Dicee Challenge - Starting Files/script.js
@@ -1,13 +1,14 @@
+let random_number1 = Math.floor(Math.random() * 6) + 1;
+let random_number2 = Math.floor(Math.random() * 6) + 1;
+let rand_img_src1 = "./images/dice" + random_number1 + ".png";
+let rand_img_src2 = "./images/dice" + random_number2 + ".png";
+let img1 = document.querySelector(".p1 img");
+let img2 = document.querySelector(".p2 img");
+let h1 = document.querySelector(".title");
+
 function Rand() {
-  let random_number1 = Math.floor(Math.random() * 6) + 1;
-  let random_number2 = Math.floor(Math.random() * 6) + 1;
-  let rand_img_src1 = "./images/dice" + random_number1 + ".png";
-  let rand_img_src2 = "./images/dice" + random_number2 + ".png";
-  let img1 = document.querySelector(".p1 img");
   img1.setAttribute("src", rand_img_src1);
-  let img2 = document.querySelector(".p2 img");
   img2.setAttribute("src", rand_img_src2);
-  let h1 = document.querySelector(".title");
 
   if (random_number1 > random_number2) {
     h1.textContent = "🚩Player 1 Wins!";
@@ -18,9 +19,15 @@ function Rand() {
   }
 }
 
+function Reset() {
+  img1.setAttribute("src", "./images/dice6.png");
+  img2.setAttribute("src", "./images/dice6.png");
+  h1.textContent = "Refresh Me!";
+}
+
 window.onload = function () {
-  let reloading = sessionStorage.getItem("reloading");
-  if (reloading) {
+  let flag = sessionStorage.getItem("reloading");
+  if (flag) {
     sessionStorage.removeItem("reloading");
     Rand();
   }
