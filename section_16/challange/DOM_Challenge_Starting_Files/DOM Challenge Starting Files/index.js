10cc80f4c66ae0a2bf8226bd9e825bfde9d10252
Muflih <mufliharifinrahmat@gmail.com>
2023-11-12 09:54:21 +0700

    add section 16

---
+++ section_16/challange/DOM_Challenge_Starting_Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,16 @@
+//section 16_2
+var third_list = document.querySelector("ul").lastElementChild;
+//section 16_4
+var first_list = document.querySelector(".list a");
+// var first_list = document.querySelector("ul").firstElementChild; //alternative
+//section 16_5
+var button = document.querySelector("button");
+//section 16_6
+var heading = document.querySelector("h1").classList.add("huge");
+
+//section 16_2
+third_list.innerHTML = "Muflih";
+//section 16_4
+first_list.style.color = "red";
+//section 16_5
+button.style.backgroundColor = "yellow";
