22687127eda52d27182709fb0b39d5552df21afc
Muflih <mufliharifinrahmat@gmail.com>
2023-11-24 05:19:04 +0700

    update drum challange

--- section_18/challange/Drum_Kit_Starting_Files/Drum Kit Starting Files/index.js
+++ section_18/challange/Drum_Kit_Starting_Files/Drum Kit Starting Files/index.js
@@ -6,10 +6,49 @@
 // );
 
 let drum = document.querySelectorAll(".set .drum");
-let tom_1 = new Audio("./sounds/tom-1.mp3");
 
 for (let i = 0; i < drum.length; i++) {
   drum[i].addEventListener("click", function () {
-    this.style.color = "white";
+    this.buttonSelection = drum[i].innerHTML;
+    makeSomeNoise(this.buttonSelection);
   });
+}
+
+document.addEventListener("keydown", function (e) {
+  makeSomeNoise(e.key);
+});
+
+function makeSomeNoise(key) {
+  switch (key) {
+    case "w":
+      this.sound = new Audio("./sounds/tom-1.mp3");
+      this.sound.play();
+      break;
+    case "a":
+      this.sound = new Audio("./sounds/tom-2.mp3");
+      this.sound.play();
+      break;
+    case "s":
+      this.sound = new Audio("./sounds/tom-3.mp3");
+      this.sound.play();
+      break;
+    case "d":
+      this.sound = new Audio("./sounds/tom-4.mp3");
+      this.sound.play();
+      break;
+    case "j":
+      this.sound = new Audio("./sounds/snare.mp3");
+      this.sound.play();
+      break;
+    case "k":
+      this.sound = new Audio("./sounds/crash.mp3");
+      this.sound.play();
+      break;
+    case "l":
+      this.sound = new Audio("./sounds/kick-bass.mp3");
+      this.sound.play();
+      break;
+    default:
+      break;
+  }
 }
