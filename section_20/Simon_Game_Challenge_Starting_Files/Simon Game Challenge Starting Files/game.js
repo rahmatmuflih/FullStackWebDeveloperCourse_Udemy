771f5c3fb0b628cd3c331148800542d588628ec8
Muflih <mufliharifinrahmat@gmail.com>
2023-11-28 18:25:41 +0700

    update game.js

--- section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
+++ section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,6 @@
 const butttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
-$(document).on("click", function () {
-  $("#" + nextSequence()).css("background-color", "white");
-  setTimeout(function () {
-    $("#" + nextSequence()).css("background-color", nextSequence());
-  }, 1);
-  nextSound(nextSequence());
-});
-
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = butttonColours[randomNumber];
