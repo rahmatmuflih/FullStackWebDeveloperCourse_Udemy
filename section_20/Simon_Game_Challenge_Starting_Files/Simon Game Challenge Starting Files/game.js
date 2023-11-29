765f5b32504a1c6d01eb08f11402421ffbf64fcd
Muflih <mufliharifinrahmat@gmail.com>
2023-11-29 10:15:13 +0700

    update sistem simon game

--- section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
+++ section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,23 @@
 const butttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
+$(document).ready(function () {
+  $(document).on("click", function () {
+    nextSequence();
+    $("#level-title").html("Level " + gamePattern.length);
+    $("#" + gamePattern[gamePattern.length - 1]).fadeOut(50);
+    $("#" + gamePattern[gamePattern.length - 1]).fadeIn(50);
+    nextSound(gamePattern[gamePattern.length - 1]);
+    // for (let i = 0; i < gamePattern.length; i++) {
+    //   setTimeout(function () {
+    //     $("#" + gamePattern[i]).fadeOut(50);
+    //     $("#" + gamePattern[i]).fadeIn(50);
+    //     nextSound(gamePattern[i]);
+    //   }, i * 1000);
+    // }
+  });
+});
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = butttonColours[randomNumber];
