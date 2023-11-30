9339328f00874edb5dfe9347688c5f6ad3cc2da4
Muflih <mufliharifinrahmat@gmail.com>
2023-11-30 12:52:05 +0700

    update game system

--- section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
+++ section_20/Simon_Game_Challenge_Starting_Files/Simon Game Challenge Starting Files/game.js
@@ -1,31 +1,91 @@
 const butttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let colour = "";
+let answer = 0;
 
 $(document).ready(function () {
   $(document).on("click", function () {
-    nextSequence();
-    $("#level-title").html("Level " + gamePattern.length);
-    $("#" + gamePattern[gamePattern.length - 1]).fadeOut(50);
-    $("#" + gamePattern[gamePattern.length - 1]).fadeIn(50);
-    nextSound(gamePattern[gamePattern.length - 1]);
-    // for (let i = 0; i < gamePattern.length; i++) {
-    //   setTimeout(function () {
-    //     $("#" + gamePattern[i]).fadeOut(50);
-    //     $("#" + gamePattern[i]).fadeIn(50);
-    //     nextSound(gamePattern[i]);
-    //   }, i * 1000);
-    // }
+    $(document).off("click");
+
+    gameStart();
   });
 });
+
+function gameStart() {
+  colour = nextSequence();
+
+  $("#level-title").text("Level " + level);
+
+  setTimeout(function () {
+    $("#" + colour).fadeOut(50);
+    $("#" + colour).fadeIn(50);
+    playSound(colour);
+  }, 1000);
+
+  $(".btn").on("click", function (e) {
+    let userChosenColour = e.target.id;
+
+    userClickedPattern.push(userChosenColour);
+
+    checkAnswer();
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    // console.log(userClickedPattern);
+  });
+}
+
+function checkAnswer() {
+  for (let i = 0; i < userClickedPattern.length; i++) {
+    console.log("cycle- " + i);
+    console.log("this is game Pattern " + gamePattern);
+    if (userClickedPattern[i] === gamePattern[i]) {
+      answer++;
+      console.log(
+        "checking " + userClickedPattern[i] + " and " + gamePattern[i]
+      );
+      console.log("this is user clicked pattern " + userClickedPattern);
+      console.log("Right");
+    } else {
+      userClickedPattern = [];
+      console.log(
+        "checking " + userClickedPattern[i] + " and " + gamePattern[i]
+      );
+      console.log("this is user clicked pattern " + userClickedPattern);
+      console.log("Wrong");
+    }
+  }
+  console.log("----- end of cycle -----");
+  if (
+    userClickedPattern.length === gamePattern.length &&
+    answer === gamePattern.length
+  ) {
+    userClickedPattern = [];
+    gameStart();
+  }
+}
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = butttonColours[randomNumber];
+
   gamePattern.push(randomChosenColour);
+  level++;
+
   return randomChosenColour;
 }
 
-function nextSound(randomColour) {
+function animatePress(currentColour) {
+  var activeButton = document.querySelector("." + currentColour);
+
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+
+function playSound(randomColour) {
   switch (randomColour) {
     case "red":
       this.sound = new Audio("./sounds/red.mp3");
